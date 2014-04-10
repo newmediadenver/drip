d8705f97a91506e68e652a2cabd077850ad67f11
Ryan McVeigh <ryanm@newmediadenver.com>
2014-04-10 11:49:56 -0600

    Basic functionality build for ajax plugin

--- js/drip.js
+++ js/drip.js
@@ -6,7 +6,8 @@
       authToken: 'abcdef12345',
       reverse: 'true',
       target: $(this),
-      delay: 60000
+      delay: '1000',
+      maxResults: 100
     }, options );
     // If setting isn't defined it will use default setting defined above as var.
     var $room = settings.room,
@@ -14,23 +15,24 @@
         $reverse = settings.reverse,
         $target = settings.target,
         $time = settings.delay,
-        url = "https://api.hipchat.com/v2/room/"+$room+"/history?auth_token="+$authToken+"&reverse="+$reverse;
+        $maxResults = settings.maxResults,
+        $url = "https://api.hipchat.com/v2/room/"+$room+"/history?auth_token="+$authToken+"&reverse="+$reverse+"&max-results="+$maxResults;
     //ajax call to HipChat API
-    $.getJSON( url, function( data ) {
+    $.getJSON( $url, function( data ) {
       //check to see if list exists then append it if it doesn't
-      if (!$target.children('.hipchat-list').length) {
-        $target.append('<ul class="hipchat-list"></ul>')
+      if (!$('#hipchat-list').length) {
+        $target.append('<ul id="hipchat-list"></ul>')
       };
       //append new messages
       for (var i in data.items) {
         var $date = data.items[i].date,
-            date = new Date($date), //convert to Date
-            $simpleDate = date.toLocaleString(), //convert to local string value
+            $jsDate = new Date($date), //convert to Date
+            $simpleDate = $jsDate.toLocaleString(), //convert to local string value
             $messageDate  = $date.replace(/[^0-9.]/g, ""), //convert to numbers
-            $prevMessageDate = $target.children('.hipchat-list .date:first').text().replace(/[^0-9.]/g, "");
+            $prevMessageDate = $('#hipchat-list .date:first').text().replace(/[^0-9.]/g, "");
         //if this message doesn't exist then prepend it to the list.
         if ($messageDate > $prevMessageDate) {
-          $target.children('.hipchat-list').prepend('<li class="hipchat-message color-'+data.items[i].color+' "><span class="user-name">'+data.items[i].from+'</span><span class="message">  '+ data.items[i].message +'  </span><span class="message-date">'+ $simpleDate +'</span><span class="date" style="display: none;">'+$date+'</span></li>');
+          $('#hipchat-list').prepend('<li class="hipchat-message color-'+data.items[i].color+' "><span class="user-name">'+data.items[i].from+'</span><span class="message">  '+ data.items[i].message +'  </span><span class="message-date">'+ $simpleDate +'</span><span class="date" style="display: none;">'+$date+'</span></li>');
         };
       }; //end for loop
     }); //end ajax
