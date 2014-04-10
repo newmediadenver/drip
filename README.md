716060cdf63aa0030ddb7e3e2a3d2d7101a45552
Ryan McVeigh <ryanm@newmediadenver.com>
2014-04-10 12:20:47 -0600

    editing README file

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
-drudnotify
-==========
+# DRIP
+##A notification widget that integrates with HipChat.
 
-A notification widget to initially integrate with hipchat.
+This widget uses a custom jQuery plugin to create a list of messages stored in your chatroom’s history. It is displayed in the same order that the messages are displayed on the HipChat application (user-name, message, date).
+
+***
+
+To use this widget simply create an html document that calls jQuery and then the drip.js file. Once this is done you can call the drip function by calling it after selecting the element you would like to append the HipChat history to. The plugin will then populate that element with a list of the rooms most recent messages.
+
+```
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>jQuery.getJSON demo</title>
+  <style>
+  img {
+    height: 100px;
+    float: left;
+  }
+  </style>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src="js/drip.js"></script>
+</head>
+<body>
+
+<div id="hipchat-info">
+
+</div>
+<script>
+  jQuery(document).ready(function($) {
+    var roomId  = '510323',
+        token   = 'hVmouQ5E6Zgj6cHzO9E0K5D4I6PE6rSLdyptPlPz';
+    $('#hipchat-info').drip({
+      room: '12345', //room id or name (default: 12345)
+      authToken: 'abcdef12345', //your API Access token (default: abcdef12345)
+      reverse: 'true', //the order you want your messages in (default: true == newest first)
+      delay: ‘60000', // the amount of time (milliseconds) between list refreshes. (default: 60000)
+      maxResults: 100 //maximum number of results you want displayed (default: 100)
+    });
+  });
+</script>
+</body>
+</html>
+```
--- js/drip.js
+++ js/drip.js
@@ -1,12 +1,12 @@
 (function ( $ ) {
-  $.fn.getHipChat = function(options){
+  $.fn.drip = function(options){
     var settings = $.extend({
     // These are the defaults.
       room: '12345',
       authToken: 'abcdef12345',
       reverse: 'true',
       target: $(this),
-      delay: '1000',
+      delay: 60000,
       maxResults: 100
     }, options );
     // If setting isn't defined it will use default setting defined above as var.
@@ -20,8 +20,8 @@
     //ajax call to HipChat API
     $.getJSON( $url, function( data ) {
       //check to see if list exists then append it if it doesn't
-      if (!$('#hipchat-list').length) {
-        $target.append('<ul id="hipchat-list"></ul>')
+      if (!$target.find('.hipchat-list').length) {
+        $target.append('<ul class="hipchat-list"></ul>')
       };
       //append new messages
       for (var i in data.items) {
@@ -29,17 +29,17 @@
             $jsDate = new Date($date), //convert to Date
             $simpleDate = $jsDate.toLocaleString(), //convert to local string value
             $messageDate  = $date.replace(/[^0-9.]/g, ""), //convert to numbers
-            $prevMessageDate = $('#hipchat-list .date:first').text().replace(/[^0-9.]/g, "");
+            $prevMessageDate = $target.find('.hipchat-list .date:first').text().replace(/[^0-9.]/g, "");
         //if this message doesn't exist then prepend it to the list.
         if ($messageDate > $prevMessageDate) {
-          $('#hipchat-list').prepend('<li class="hipchat-message color-'+data.items[i].color+' "><span class="user-name">'+data.items[i].from+'</span><span class="message">  '+ data.items[i].message +'  </span><span class="message-date">'+ $simpleDate +'</span><span class="date" style="display: none;">'+$date+'</span></li>');
+          $target.find('.hipchat-list').prepend('<li class="hipchat-message color-'+data.items[i].color+' "><span class="user-name">'+data.items[i].from+'</span><span class="message">  '+ data.items[i].message +'  </span><span class="message-date">'+ $simpleDate +'</span><span class="date" style="display: none;">'+$date+'</span></li>');
         };
       }; //end for loop
     }); //end ajax
     //If delay is 0 then do not rerun ajax call to HipChat API.
     if ($time != 0) {
       //check the HipChat API every x milliseconds.
-      setInterval(function() { $target.getHipChat({ room: $room, authToken: $authToken, reverse: $reverse, delay: $time }); }, $time);
+      setInterval(function() { $target.drip({ room: $room, authToken: $authToken, reverse: $reverse, delay: $time }); }, $time);
     };
   }
 }( jQuery ));
